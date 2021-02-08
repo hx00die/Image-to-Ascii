bc1642ed489ed1b28043d2137de3964a18c993ee
hx00die <74320169+hx00die@users.noreply.github.com>
2021-02-08 10:21:48 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,14 +7,18 @@ extern "C" {
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 }
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
 
 bool load_image(std::vector<unsigned char>& image, const std::string& filename, int& x, int& y)
 {
     int n;
     unsigned char* data = stbi_load(filename.c_str(), &x, &y, &n, 4);
+    unsigned char* sized_data = data;
+    stbir_resize_uint8(data, x, y, 0, sized_data, 100, 100, 0, 4);
     if (data != nullptr)
     {
-        image = std::vector<unsigned char>(data, data + x * y * 4);
+        image = std::vector<unsigned char>(sized_data, sized_data + 100 * 100 * 4);
     }
     stbi_image_free(data);
     return (data != nullptr);
@@ -41,7 +45,7 @@ void convert2ascii(std::vector<unsigned char> image, const size_t &RGBA, int wid
     file.open("image.txt", std::ios::out);
     if (file.is_open()) {
         for (int j = 0; j < height; j++) {
-            for (int i = 0; i < height; i++) {
+            for (int i = 0; i < width; i++) {
                 size_t index = RGBA * (j * width + i);
                 float r = static_cast<int>(image[index + 0]) / 255.0f;
                 float g = static_cast<int>(image[index + 1]) / 255.0f;
@@ -50,7 +54,7 @@ void convert2ascii(std::vector<unsigned char> image, const size_t &RGBA, int wid
                 charector = BestCharector(colour);
                 file << charector;
             }
-            file << charector << '\n';
+            file << '\n';
         }
         file.close();
     }
@@ -63,6 +67,8 @@ int main()
     std::cin >> filename;
 
     int width, height;
+    int fixedWidth = 100, fixedHeight = 100;
+    
     std::vector<unsigned char> image;
     bool success = load_image(image, filename, width, height);
     if (!success)
@@ -73,12 +79,14 @@ int main()
 
     std::cout << "Image width = " << width << '\n';
     std::cout << "Image height = " << height << '\n';
-
+    std::cout << "Dimensions changed to 100x100\n";
+    
     const size_t RGBA = 4;
-    convert2ascii(image, RGBA, width, height);
+    convert2ascii(image, RGBA, 100, 100);
     std::cout << "Done!\n";
+    
     char key;
-    std::cout << "press a key and then enter to close the window. ";
+    std::cout << "Press a key and then enter to close the window. ";
     std::cin >> key;
 
     return 0;
