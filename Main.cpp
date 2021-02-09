5ce1dc881ec3bb715c64a0574ab117df1b0322d4
hx00die <74320169+hx00die@users.noreply.github.com>
2021-02-09 11:55:26 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -24,35 +24,20 @@ bool load_image(std::vector<unsigned char>& image, const std::string& filename, 
     return (data != nullptr);
 }
 
-char BestCharector(float colour) {
-    switch ((int)(colour * 11.0f)) {
-    case 0: return '@';
-    case 1: return '#';
-    case 2: return '%'; 
-    case 3: return 'x'; 
-    case 4: return 'o'; 
-    case 5: return ';'; 
-    case 6: return ':'; 
-    case 7: return ','; 
-    case 8: return '.'; 
-    case 9: return ' ';
-    }
-}
-
 void convert2ascii(std::vector<unsigned char> image, const size_t &RGBA, int width, int height) {
-    char charector;
+    std::vector <char> charectors = { '@' ,'#' ,'%' ,'x' ,'o' ,';' ,':' ,',' ,'.' ,' ' };
     std::fstream file;
     file.open("image.txt", std::ios::out);
     if (file.is_open()) {
         for (int j = 0; j < height; j++) {
             for (int i = 0; i < width; i++) {
                 size_t index = RGBA * (j * width + i);
-                float r = static_cast<int>(image[index + 0]) / 255.0f;
-                float g = static_cast<int>(image[index + 1]) / 255.0f;
-                float b = static_cast<int>(image[index + 2]) / 255.0f;
+                float const scalingConstant = 255 / 10;
+                float r = static_cast<int>(image[index + 0]) / scalingConstant;
+                float g = static_cast<int>(image[index + 1]) / scalingConstant;
+                float b = static_cast<int>(image[index + 2]) / scalingConstant;
                 float colour = r * 0.3 + g * 0.59 + b * 0.11;
-                charector = BestCharector(colour);
-                file << charector;
+                file << charectors[(int)colour];
             }
             file << '\n';
         }
@@ -68,7 +53,6 @@ int main()
 
     int width, height;
     int fixedWidth = 100, fixedHeight = 100;
-    
     std::vector<unsigned char> image;
     bool success = load_image(image, filename, width, height);
     if (!success)
@@ -80,11 +64,9 @@ int main()
     std::cout << "Image width = " << width << '\n';
     std::cout << "Image height = " << height << '\n';
     std::cout << "Dimensions changed to 100x100\n";
-    
     const size_t RGBA = 4;
     convert2ascii(image, RGBA, 100, 100);
     std::cout << "Done!\n";
-    
     char key;
     std::cout << "Press a key and then enter to close the window. ";
     std::cin >> key;
